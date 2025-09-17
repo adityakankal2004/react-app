2cd6a9d737ecf5e5b32b450670e975252c332d64
aditya kankal <adityabuisness12@gmail.com>
2025-09-17 14:51:09 +0530

    new

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,28 +2,15 @@ import "./Product.css"
 
 function Product({title,price,features=[],features2}) {
     // const list = features.map((feature) => <li>{feature}</li>);
-    if(price >= 50000){
+        
         return (
         <div className = "Product">
             <h3>{title}</h3>
             <h3>Price-{price}</h3>
             <p>{features}</p>
-            <p>Discount of 5%</p>
+            {price > 30000 ? <p>"Discount of 5%"</p> : null}
         </div>
     );
-    } else {
-        return (
-        <div className = "Product">
-            <h3>{title}</h3>
-            <h3>Price-{price/2}</h3>
-            <p>{features}</p>
-            <p>
-                {features.map((feature) => (
-                    <li>{feature}</li>
-                ))}</p>
-        </div>
-    );
-    }
     
 }
 
