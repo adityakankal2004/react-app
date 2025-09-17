4b96059b551bcc7de658190f7649190fef291c3a
aditya kankal <adityabuisness12@gmail.com>
2025-09-17 18:31:07 +0530

    Amazon cards stage -3

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,10 +4,11 @@ import Price from "./Price";
 function Product({title,idx}) {
     let oldPrices = ["12,455","81,000","61,000","74,000"];
     let newPrices = ["10,500","75,999","57,899","69,999"];
+    let description = ["Slim Elegant and fun","The greatest MacBook ever","Gaming at it's best","Best for designers and coders"];
         return (
         <div className="Product">
             <h4>{title}</h4>
-            <p>Description</p>
+            <p>{description[idx]}</p>
             <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
         </div>
     );
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,14 +1,17 @@
 import Product from "./Product.jsx";
 
 function ProductTab () {
-
+    let styles = {
+        display:"flex",
+        flexWrap:"wrap",
+    };
     return (
-        <>
+        <div style={styles}>
             <Product title="Asus SlimBook V405" idx={0}/>
             <Product title="MacBook M4 air" idx={1}/>
             <Product title="Acer Predator Helios Neo 16" idx={2}/>
             <Product title="Asus Vivobook" idx={3}/>
-        </>
+        </div>
     );
 }
 
