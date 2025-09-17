de95af21e4f3d2c8a04e8a002adf42e522c4cfa5
aditya kankal <adityabuisness12@gmail.com>
2025-09-17 18:14:05 +0530

    Amazon cards stage-2

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,14 @@
 import "./Product.css"
 import Price from "./Price";
 
-function Product({title}) {
+function Product({title,idx}) {
     let oldPrices = ["12,455","81,000","61,000","74,000"];
     let newPrices = ["10,500","75,999","57,899","69,999"];
         return (
         <div className="Product">
             <h4>{title}</h4>
             <p>Description</p>
-            <Price/>
+            <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
         </div>
     );
     
